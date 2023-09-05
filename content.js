80aa3c33a076df6389411abfc2a7b711984db7a1
C-Alexis4414 <a.chentre@protonmail.com>
2023-09-05 15:18:06 +0200

    Method GET

--- content.js
+++ content.js
@@ -1,2 +1,10 @@
-let test = document.querySelector("#activeRadioName").innerHTML = "I DID IT HAHAHA";
-console.log('test : ' + test);
+function reader() {
+    const options = {method: 'GET'};
+
+    fetch('https://api.voicerss.org/?key=4da2191a46b34962be23655d83ee1d71&hl=en-us&c=MP3&src=Hello%2C%20world!', options)
+    .then(response => response.text())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+}
+
+reader()
