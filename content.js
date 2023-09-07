7a7def76ffefaf388ea730d72d4a6d56224c797a
root <root@LAPTOP-I4U4E0I4>
2023-09-07 15:47:21 +0200

    wip2

--- content.js
+++ content.js
@@ -1,16 +1,47 @@
+let highlightedText;
+
 //Function reader => get under text the mp3 from highlightedText
 function reader() {
     const options = {method: 'GET'};
-
-    fetch(`https://api.voicerss.org/?key=4da2191a46b34962be23655d83ee1d71&hl=fr-fr&c=MP3&src=${highlightedText}`, options)
-    .then(response => response.text())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+    // Fetch the original image
+fetch(`https://api.voicerss.org/?key=4da2191a46b34962be23655d83ee1d71&hl=fr-fr&c=MP3&src=${highlightedText}`)
+// Retrieve its body as ReadableStream
+.then((response) => {
+  const reader = response.body.getReader();
+  return new ReadableStream({
+    start(controller) {
+      return pump();
+      function pump() {
+        return reader.read().then(({ done, value }) => {
+          // When no more data needs to be consumed, close the stream
+          if (done) {
+            controller.close();
+            return;
+          }
+          // Enqueue the next data chunk into our target stream
+          controller.enqueue(value);
+          return pump();
+        });
+      }
+    },
+  });
+})
+// Create a new response out of the stream
+.then((stream) => new Response(stream))
+// Create an object URL for the response
+.then((response) => response.blob())
+.then((blob) => URL.createObjectURL(blob))
+// Update image
+.then((url) => console.log((document.querySelector("audio").src = url)))
+.catch((err) => console.error(err));
+    // fetch(`https://api.voicerss.org/?key=4da2191a46b34962be23655d83ee1d71&hl=fr-fr&c=MP3&src=${highlightedText}`, options)
+    // .then(response => response.text())
+    // .then(response => console.log(response))
+    // .catch(err => console.error(err));
 }
 
 //Function to highligth the text in the web page and return the highlighted text
 document.addEventListener("mouseup", () => { //the user highlight a text and when mouse up, the function is launch
-let highlightedText;
 highlightedText = window.getSelection().toString().replace(/ /g, "%2C%20"); //the highlighted text is put into string and replace the space by "%2C%20"
 console.log(highlightedText);
 reader();
