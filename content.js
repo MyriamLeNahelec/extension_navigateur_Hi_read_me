88dc868be50fc69a86b214df24ffd3b9e7239627
root <root@LAPTOP-I4U4E0I4>
2023-09-11 10:22:59 +0200

    monday cleaning

--- content.js
+++ content.js
@@ -1,17 +1,11 @@
-const cleVoixAPI = process.env.VOICE_API_KEY;
-
 let highlightedText;
-var header = document.createElement("pedro");
-header.textContent = "Ajout du texte qu'on veut entendre";
-document.body.appendChild(header);
-
-// let highlightedText;
 
 //Function reader => get under text the mp3 from highlightedText
 function reader() {
     const options = {method: 'GET'};
     // Fetch the original image
-fetch(`https://api.voicerss.org/?key=`${cleVoixAPI}`&hl=fr-fr&c=MP3&src=${highlightedText}`, options)
+fetch(`https://api.voicerss.org/?key=4da2191a46b34962be23655d83ee1d71&hl=fr-fr&c=MP3&src=${highlightedText}`, options)
+
 // Retrieve its body as ReadableStream
 .then((response) => {
   const reader = response.body.getReader();
@@ -41,10 +35,6 @@ fetch(`https://api.voicerss.org/?key=`${cleVoixAPI}`&hl=fr-fr&c=MP3&src=${highli
 // Update image
 .then((url) => console.log((document.querySelector("audio").src = url)))
 .catch((err) => console.error(err));
-    // fetch(`https://api.voicerss.org/?key=4da2191a46b34962be23655d83ee1d71&hl=fr-fr&c=MP3&src=${highlightedText}`, options)
-    // .then(response => response.text())
-    // .then(response => console.log(response))
-    // .catch(err => console.error(err));
 }
 
 //Function to highligth the text in the web page and return the highlighted text
@@ -54,21 +44,19 @@ console.log(highlightedText);
 reader();
 });
 
+//Code pour plus tard
 
+//Function to hide the API key
+// const cleVoixAPI = process.env.VOICE_API_KEY;
 
+//Function to add a mp3 reader on the web page
+// let highlightedText;
+// var header = document.createElement("pedro");
+// header.textContent = "Ajout du texte qu'on veut entendre";
+// document.body.appendChild(header);
 
-
-
-
-
-
-
-
-
-
-
-// reader()
-
-//function to send the text to the voice reader & receive the MP3 
-
-//function to send the MP3 file to the MP3 reader (via HTML)
+// Old fetch API keep just in case
+// fetch(`https://api.voicerss.org/?key=4da2191a46b34962be23655d83ee1d71&hl=fr-fr&c=MP3&src=${highlightedText}`, options)
+// .then(response => response.text())
+// .then(response => console.log(response))
+// .catch(err => console.error(err));
